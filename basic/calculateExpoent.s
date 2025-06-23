ae7c4656c64fbc9c8fee1ec9ec15c3cd2d947ff6
AnaLuizanc <analuizanc13@gmail.com>
2025-06-23 18:44:55 -0300

    docs(calculateExpoent): add comments for clarity in assembly code

--- basic/calculateExpoent.s
+++ basic/calculateExpoent.s
@@ -5,16 +5,16 @@ res: .long 1
 .section .text
 .globl _start
 _start:
-    movl base, %ebx
-    movl expoente, %ecx
-    movl res, %eax
+    movl base, %ebx        # Carrega base em ebx
+    movl expoente, %ecx    # Carrega expoente em ecx
+    movl res, %eax         # Inicializa eax com res (acumulador)
 
 _start_while:
-    addl $0, %ecx
-    jz _end
-    mull %ebx
-    decl %ecx
-    jmp _start_while
+    addl $0, %ecx          # Atualiza flags para ecx (necessário para jz)
+    jz _end                # Se ecx == 0, termina o laço
+    mull %ebx              # eax = eax * ebx (res = res * base)
+    decl %ecx          
+    jmp _start_while     
 
 _end:
     movl %eax, %edi
