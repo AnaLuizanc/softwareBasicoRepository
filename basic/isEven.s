001c71259fb644ebd3bf00d8832ad42059d8daf7
AnaLuizanc <analuizanc13@gmail.com>
2025-06-23 19:59:08 -0300

    docs(isEven): add comments for clarity

--- basic/isEven.s
+++ basic/isEven.s
@@ -3,16 +3,16 @@ numero: .long 1
 .section .text
 .globl _start
 _start:
-    movl numero, %eax
-    movl $2, %ebx
-    divl %ebx
-    cmpl $0, %edx
-    je _is_equal
-    movl $0, %edi
-    movq $60, %rax
+    movl numero, %eax     # carrega o valor de 'numero' em eax
+    movl $2, %ebx         # carrega 2 em ebx (divisor)
+    divl %ebx             # divide eax por ebx; quociente em eax, resto em edx
+    cmpl $0, %edx         # compara o resto (edx) com 0
+    je _is_equal          # se resto == 0, número é par
+    movl $0, %edi         # se ímpar, retorna 0
+    movq $60, %rax  
     syscall
 
 _is_equal:
-    movl $1, %edi
-    movq $60, %rax
-    syscall 
+    movl $1, %edi         # se par, retorna 1
+    movq $60, %rax 
+    syscall
